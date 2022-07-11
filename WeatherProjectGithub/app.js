3e5418a700e39c09fb45930cee50646318cea268
Sai Geethika Gudapati <79441278+SaiGeethika-G@users.noreply.github.com>
2022-07-11 18:30:36 +0530

    Update app.js
    Using weather API, parsed the required data for the city desired by user. Extracted the city name from html using body-parser. Displayed the data using a post method.

--- WeatherProjectGithub/app.js
+++ WeatherProjectGithub/app.js
@@ -1,19 +1,26 @@
 const express=require("express");
+const bodyParser=require("body-parser");
 const https=require("https");
 const { stringify } = require("querystring");
 const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/",function(req,res){
     // res.send("Weather data will be displayed here!");
     var url="https://api.openweathermap.org/data/2.5/weather?units=metric&q=India&appid=b29a3bb4c0337551fd087b655d4ef7db";
     https.get(url,function(response){
         console.log(response.statusCode);
         response.on("data",function(data){
+            res.set("Content-Type", "text/html");
             const weatherdata=JSON.parse(data);
             const temp=weatherdata.main.temp;
-            console.log(weatherdata);
-            console.log(weatherdata.main.temp_max);
-            console.log(weatherdata.weather[0].description);
-            res.send("<h1><i>The temperature in India is "+temp+" degrees celcius</i></h1>");
+            const weatherDescription=weatherdata.weather[0].description;
+            const icon=weatherdata.weather[0].icon;
+            const imgurl="http://openweathermap.org/img/wn/"+icon+"@2x.png";
+            res.write("The weather is currently "+weatherDescription);
+            res.write("<h1>The temperature in India is "+temp+" degrees celcius</h1>");
+            res.write("<img src="+imgurl+" alt="+icon+">");
+            res.send();
         });
         //To stringify data
         // const stdata=JSON.stringify({gfdgaags:"gdgdsgf",dggfdgfe:"hdggd"});
@@ -21,6 +28,32 @@ app.get("/",function(req,res){
 
     });
 });
+
+//For entered city
+app.get("/city",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+app.post("/city",function(req,res){
+    var city=req.body.city;
+    var apikey="b29a3bb4c0337551fd087b655d4ef7db";
+    var url="https://api.openweathermap.org/data/2.5/weather?units=metric&q="+city+"&appid="+apikey;
+    https.get(url,function(response){
+        console.log(response.statusCode);
+        response.on("data",function(data){
+            res.set("Content-Type", "text/html");
+            const userCityData=JSON.parse(data);
+            const temp=userCityData.main.temp;
+            const weatherDescription=userCityData.weather[0].description;
+            const icon=userCityData.weather[0].icon;
+            const imgurl="http://openweathermap.org/img/wn/"+icon+"@2x.png";
+            res.write("The weather is currently "+weatherDescription);
+            res.write("<h1>The temperature in "+city+" is "+temp+" degrees celcius</h1>");
+            res.write("<img src="+imgurl+" alt="+icon+">");
+            res.send();
+        })
+    })
+});
+
 app.listen(3000,function(){
     console.log("Server started successfully at port 3000!");
 });
